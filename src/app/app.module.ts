d045830b57410b428cb1c0b2b62c43a9afea06bc
andyblacknred <andyblacknred@gmail.com>
2020-09-27 18:58:07 +0300

    finished main-screen & header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
+import { MainScreenComponent } from './components/main-screen/main-screen.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CalendarComponent,
+    MainScreenComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
