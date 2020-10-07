bcf852a81f05f55aee66532e5b5f49bb3ede7cf6
andyblacknred <andyblacknred@gmail.com>
2020-10-07 09:38:50 +0300

    acquaintance with ngrx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
 import { HeaderComponent } from './components/header/header.component';
+
+import { increment, decrement, reset } from './store/store.actions';
+
 
 @Component({
   selector: 'app-root',
@@ -8,4 +14,22 @@ import { HeaderComponent } from './components/header/header.component';
 })
 export class AppComponent {
   title = 'werdevs-test';
+
+  count$: Observable<number>;
+
+  constructor(private store: Store<{ count: number }>) {
+    this.count$ = store.select('count');
+  }
+
+  increment() {
+    this.store.dispatch(increment());
+  }
+
+  decrement() {
+    this.store.dispatch(decrement());
+  }
+
+  reset() {
+    this.store.dispatch(reset());
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { StoreModule } from '@ngrx/store';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { CalendarComponent } from './components/calendar/calendar.component';
 import { MainScreenComponent } from './components/main-screen/main-screen.component';
 import { HomeComponent } from './pages/home/home.component';
+
+
+import { counterReducer } from './store/store.reducer';
 
 @NgModule({
   declarations: [
@@ -18,7 +23,8 @@ import { HomeComponent } from './pages/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({ count: counterReducer })
   ],
   providers: [],
   bootstrap: [AppComponent]
